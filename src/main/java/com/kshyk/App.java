3e6d9b7d5d10440f815820661abcf51cccbac281
kshyk <jj.turkiewicz@gmail.com>
2018-04-14 01:53:06 +0200

    test

--- src/main/java/com/kshyk/App.java
+++ src/main/java/com/kshyk/App.java
@@ -1,4 +1,15 @@
 package com.kshyk;
 
+import java.util.UUID;
+
 public class App {
+
+    public static void main(String[] args) {
+        App obj = new App();
+        System.out.println("Unique ID : " + obj.generateUniqueKey());
+    }
+
+    String generateUniqueKey() {
+        return UUID.randomUUID().toString();
+    }
 }
--- src/test/java/com/kshyk/AppTest.java
+++ src/test/java/com/kshyk/AppTest.java
@@ -1,4 +1,12 @@
 package com.kshyk;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class AppTest {
+
+    @Test
+    public void testLengthOfTheUniqueKey() {
+        Assert.assertEquals(36, new App().generateUniqueKey().length());
+    }
 }
