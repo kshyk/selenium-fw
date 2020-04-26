0b7dec305ac180e60bd2c46358b9dae06b16c985
kshyk <jj.turkiewicz@gmail.com>
2020-04-26 18:23:59 +0200

    - put key, secret env's to remote config

--- src/test/java/com/kshyk/tests/base/TestCase.java
+++ src/test/java/com/kshyk/tests/base/TestCase.java
@@ -9,7 +9,9 @@ public abstract class TestCase {
     @BeforeAll
     protected void setup() {
         Configuration.timeout = 5000;
-//        Configuration.remote = "http://localhost:4444/wd/hub";
+        var key = System.getProperty("TESTINGBOT_KEY");
+        var secret = System.getProperty("TESTINGBOT_SECRET");
+        Configuration.remote = "https://" + key + ":" + secret + "@hub.testingbot.com/wd/hub";
         Configuration.headless = false;
         Configuration.startMaximized = true;
     }
