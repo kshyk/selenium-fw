d136f174288fb2ba65fbd1271fd4494c95422a7c
kshyk <jj.turkiewicz@gmail.com>
2020-06-15 23:21:33 +0200

    - TestWatcher and TestingbotREST implementation to send test details back to TestingBot

--- src/test/java/com/kshyk/tests/base/TestCase.java
+++ src/test/java/com/kshyk/tests/base/TestCase.java
@@ -1,13 +1,21 @@
 package com.kshyk.tests.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.testingbot.testingbotrest.TestingbotREST;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.util.Map;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public abstract class TestCase {
+public abstract class TestCase implements TestWatcher {
     private static final String KEY = System.getenv("TESTINGBOT_KEY");
     private static final String SECRET = System.getenv("TESTINGBOT_SECRET");
+    private final TestingbotREST tbREST = new TestingbotREST(KEY, SECRET);
 
     @BeforeAll
     protected void setup() {
@@ -15,5 +23,19 @@ public abstract class TestCase {
         Configuration.remote = "https://" + KEY + ":" + SECRET + "@hub.testingbot.com/wd/hub";
         Configuration.headless = false;
         Configuration.startMaximized = true;
+    }
+
+    @Override
+    public void testFailed(ExtensionContext context, Throwable cause) {
+        tbREST.updateTest(getSessionId(), Map.of("success", false));
+    }
+
+    @Override
+    public void testSuccessful(ExtensionContext context) {
+        tbREST.updateTest(getSessionId(), Map.of("success", true));
+    }
+
+    private String getSessionId() {
+        return ((RemoteWebDriver) WebDriverRunner.getWebDriver()).getSessionId().toString();
     }
 }
