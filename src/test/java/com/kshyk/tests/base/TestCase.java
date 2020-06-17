1deb91ef8c357c66de8b76b9e44af4e4907f1af0
kshyk <jj.turkiewicz@gmail.com>
2020-06-17 20:35:04 +0200

    - TestWatcher and TestingbotREST implementation to send test details back to TestingBot - part3 - code optimized

--- src/test/java/com/kshyk/tests/base/TestCase.java
+++ src/test/java/com/kshyk/tests/base/TestCase.java
@@ -12,14 +12,12 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.Map;
-
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class TestCase implements TestWatcher {
     private static final String KEY = System.getenv("TESTINGBOT_KEY");
     private static final String SECRET = System.getenv("TESTINGBOT_SECRET");
     private final TestingbotREST tbREST = new TestingbotREST(KEY, SECRET);
-    private TestingbotTest test;
+    private TestingbotTest tbTest;
 
     @BeforeAll
     protected void setup() {
@@ -30,30 +28,34 @@ public abstract class TestCase implements TestWatcher {
     }
 
     @BeforeEach
-    protected void setupTestingbotTest(){
-        test = new TestingbotTest();
-        test.setSessionId(getSessionId());
+    protected void setupTestingbotTest() {
+        tbTest = new TestingbotTest();
+        tbTest.setSessionId(getSessionId());
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
-        test.setSuccess(false);
-        test.setStatusMessage(cause.getMessage());
-        test.setName(context.getRequiredTestMethod().getName());
+        tbTest.setSuccess(false);
+        tbTest.setStatusMessage(cause.getMessage());
+        tbTest.setName(getTestMethodName(context));
     }
 
     @Override
     public void testSuccessful(ExtensionContext context) {
-        test.setSuccess(true);
-        test.setName(context.getRequiredTestMethod().getName());
+        tbTest.setSuccess(true);
+        tbTest.setName(getTestMethodName(context));
     }
 
     @AfterEach
-    public void sendBackTestStatus(){
-        tbREST.updateTest(test);
+    public void sendBackTestStatus() {
+        tbREST.updateTest(tbTest);
     }
 
     private String getSessionId() {
         return ((RemoteWebDriver) WebDriverRunner.getWebDriver()).getSessionId().toString();
+    }
+
+    private String getTestMethodName(ExtensionContext context) {
+        return context.getRequiredTestMethod().getName();
     }
 }
