490c194c85f80f135c0c645cb6c4c00fe52b4523
kshyk <jj.turkiewicz@gmail.com>
2020-07-07 09:53:47 +0200

    - hot-fix for alert prompt

--- src/test/java/com/kshyk/tests/gui/the_internet_herokuapp_com/ContextMenuTests.java
+++ src/test/java/com/kshyk/tests/gui/the_internet_herokuapp_com/ContextMenuTests.java
@@ -2,14 +2,18 @@ package com.kshyk.tests.gui.the_internet_herokuapp_com;
 
 import com.kshyk.helpers.theinternetherokuapp.ContextMenuPageHelper;
 import com.kshyk.tests.base.TestCase;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
 
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.switchTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ContextMenuTests extends TestCase {
+    private Alert alert;
+
     @BeforeAll
     public void openContextMenuPage() {
         open("http://the-internet.herokuapp.com/context_menu");
@@ -18,6 +22,13 @@ public class ContextMenuTests extends TestCase {
     @Test
     public void contextClickShouldTriggerAlert() {
         ContextMenuPageHelper.contextClickOnHotSpot();
-        assertEquals("You selected a context menu", switchTo().alert().getText());
+        alert = switchTo().alert();
+        assertEquals("You selected a context menu", alert.getText());
+    }
+
+    @AfterAll
+    public void closeAlertPrompt() {
+        if (alert != null)
+            alert.accept();
     }
 }
