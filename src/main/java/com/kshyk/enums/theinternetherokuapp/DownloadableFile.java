1b22d63d1a539979ce85c0a23c7f0bf802939542
kshyk <jj.turkiewicz@gmail.com>
2020-07-15 12:01:51 +0200

    - removed unnecessary files and enum values

--- src/main/java/com/kshyk/enums/theinternetherokuapp/DownloadableFile.java
+++ src/main/java/com/kshyk/enums/theinternetherokuapp/DownloadableFile.java
@@ -3,24 +3,7 @@ package com.kshyk.enums.theinternetherokuapp;
 import com.kshyk.common.CommonUtil;
 
 public enum DownloadableFile {
-    BILD("bild.png"),
-    BOOK("book.pdf"),
-    BOT_LOGO_ICON("bot-logo-icon.png"),
-    CARS_0002("cars_0002.jpg"),
-    CARS_0062("cars_0062.jpg"),
-    DOWNLOAD("download.png"),
-    FILE_UPLOAD("fileupload.ts"),
-    HANDY_NUMMER("handynummer.png"),
-    LOGO("logo.jpg"),
-    LUMINOS_LOGO("luminoslogo.png"),
-    NEW_TEXT_DOCUMENT("New Text Document.txt"),
-    NOT_EMPTY("not_empty.txt"),
-    SHELBY("Shelby.jpg"),
-    SOME_FILE("some-file.txt"),
-    UPLOAD_NOTEPAD("UploadNotepad.txt"),
-    URL_MOBILE("url_mobile.txt"),
-    WEBDRIVER_IO("webdriverIO.png"),
-    ZADANIA("ZADANIA.docx");
+    NOT_EMPTY("not_empty.txt");
 
     private final String fileName;
 
