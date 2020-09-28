a22087629a5ec574c51d8eafadbf78e67eb7149f
kshyk <jj.turkiewicz@gmail.com>
2020-09-28 19:23:25 +0200

    - split hover tests into parametrized ones (codacy fix)

--- src/test/java/com/kshyk/tests/gui/the_internet_herokuapp_com/HoversPageTests.java
+++ src/test/java/com/kshyk/tests/gui/the_internet_herokuapp_com/HoversPageTests.java
@@ -4,9 +4,13 @@ import com.kshyk.helpers.theinternetherokuapp.HoversPageHelper;
 import com.kshyk.interfaces.PageContent;
 import com.kshyk.tests.base.TestCase;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,12 +21,16 @@ class HoversPageTests extends TestCase {
         open("http://the-internet.herokuapp.com/hovers");
     }
 
-    @Test
-    public void additionalInfoShouldAppearOnImageHover() {
+    @ParameterizedTest(name = "{1}AdditionalInfoShouldAppearOnImageHover")
+    @MethodSource("mapSource")
+    public void additionalInfoShouldAppearOnImageHover(int index, String user) {
+        HoversPageHelper.mouseOverImageByIndex(index);
+        assertTrue(PageContent.getContentText().contains("name: " + user));
+    }
+
+    private Stream<Arguments> mapSource() {
         var users = List.of("user1", "user2", "user3");
-        users.forEach(user -> {
-            HoversPageHelper.mouseOverImageByIndex(users.indexOf(user));
-            assertTrue(PageContent.getContentText().contains("name: " + user));
-        });
+        return IntStream.range(0, users.size())
+                .mapToObj(index -> Arguments.arguments(index, users.get(index)));
     }
 }
