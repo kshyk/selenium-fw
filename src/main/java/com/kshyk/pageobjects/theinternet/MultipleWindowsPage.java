5889796d02c0b5b839ceb8fe0054f3d53a55e3e8
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-09-22 23:30:31 +0200

    fix: for multiple window test

--- src/main/java/com/kshyk/pageobjects/theinternet/MultipleWindowsPage.java
+++ src/main/java/com/kshyk/pageobjects/theinternet/MultipleWindowsPage.java
@@ -1,11 +1,15 @@
 package com.kshyk.pageobjects.theinternet;
 
 import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Platform;
 
 import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
 import static com.codeborne.selenide.Selenide.switchTo;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.Keys.COMMAND;
+import static org.openqa.selenium.Keys.CONTROL;
 
 public class MultipleWindowsPage implements PageContent {
     public static MultipleWindowsPage open() {
@@ -19,7 +23,9 @@ public class MultipleWindowsPage implements PageContent {
     }
 
     public void closeNewWindow() {
-        getWebDriver().close();
+        var currentPlatform = Platform.fromString(System.getProperty("os.name"));
+        boolean isMacOS = Platform.MAC.is(currentPlatform);
+        actions().sendKeys(Keys.chord(isMacOS ? COMMAND : CONTROL, "W")).perform();
         switchTo().window(0);
     }
 }
