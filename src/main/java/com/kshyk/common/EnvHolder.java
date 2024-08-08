80201f93bafd81e319d5877b857cf450a5a65326
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2024-08-08 22:09:21 +0200

    fix java:S6437 security issues

--- src/main/java/com/kshyk/common/EnvHolder.java
+++ src/main/java/com/kshyk/common/EnvHolder.java
@@ -4,6 +4,7 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class EnvHolder {
+    public static final String ADMIN_PASSWORD = System.getenv("ADMIN_PASSWORD");
     public static final String KEY = System.getenv("TESTINGBOT_KEY");
     public static final String SECRET = System.getenv("TESTINGBOT_SECRET");
 }
--- src/main/java/com/kshyk/pageobjects/theinternet/BasicAuthPage.java
+++ src/main/java/com/kshyk/pageobjects/theinternet/BasicAuthPage.java
@@ -4,10 +4,11 @@ import com.codeborne.selenide.BasicAuthCredentials;
 import com.codeborne.selenide.Selenide;
 
 import static com.codeborne.selenide.AuthenticationType.BASIC;
+import static com.kshyk.common.EnvHolder.ADMIN_PASSWORD;
 
 public class BasicAuthPage implements PageContent {
     public static BasicAuthPage open() {
-        var credentials = new BasicAuthCredentials("admin", "admin");
+        var credentials = new BasicAuthCredentials("admin", ADMIN_PASSWORD);
         Selenide.open("/basic_auth", BASIC, credentials);
         return new BasicAuthPage();
     }
